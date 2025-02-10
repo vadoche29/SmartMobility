c90cfb8d742dc5181dc32fc8e2eb9f1aadc005ef
vadoche29 <156700272+vadoche29@users.noreply.github.com>
2025-02-10 17:28:50 +0100

    ajoute de route selon le code de yoel

--- python/tsp_graph_init.py
+++ python/tsp_graph_init.py
@@ -33,6 +33,32 @@ class Graph:
 				ligne.append(lieu1.calculate_distance(lieu2))
 			self.matrice_od.append(ligne)
 
+	def calcul_distance_route(liste_lieux):
+		distance_totale = 0
+		for i in range(len(liste_lieux)-1):
+			distance_totale += liste_lieux[i].calculate_distance(liste_lieux[i+1])
+		distance_totale += liste_lieux[-1].calculate_distance(liste_lieux[0])
+		print(distance_totale)
+		return distance_totale
+
+
+class Route: 
+	def __init__(self, distance_totale, ordre_init=None):
+		if ordre_init is None:
+			self.ordre = [0]
+			self.ordre.extend(random.sample(range(1, nb_lieux)), nb_lieux-1)
+			self.odre.append(0)
+		else:
+			self.ordre = ordre_init[:]
+
+		self.distance_totale = Graph.calcul_distance_route(Graph.liste_lieux)
+
+	def compare(self,route):
+		if self.distance_totale < route.distance_totale:
+			return self
+		else :
+			return route
+		
 	"""def afficher_matrice_od(self):
 		for ligne in self.matrice_od:
 			print(ligne)
