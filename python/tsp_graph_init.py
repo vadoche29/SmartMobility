715fadaa12f8ce8c04578cb50685c85692f6ded3
louisehnrd <134003016+louisehnrd@users.noreply.github.com>
2025-02-10 16:43:55 +0100

    test graph faux

--- python/tsp_graph_init.py
+++ python/tsp_graph_init.py
@@ -1,4 +1,11 @@
-import math
+import numpy
+import random
+import csv
+
+LARGEUR = 800
+HAUTEUR = 600
+NB_LIEUX = None
+
 
 class Lieu :
     def __init__(self,nom,x,y):
@@ -7,12 +14,20 @@ class Lieu :
         self.y=y
 
     def calculate_distance(self,autre_lieu):
-        distance=math.sqrt((self.x-autre_lieu.x)**2+(self.y-autre_lieu.y)**2)
+        distance=numpy.sqrt((self.x-autre_lieu.x)**2+(self.y-autre_lieu.y)**2)
 
         return distance
 
 
 
-#class Graph :
+class Graph :
+    def __init__(self,liste_lieux=None):
+        if liste_lieux:
+            self.liste_lieux=liste_lieux
+        else :
+            liste_lieux=[]
+            for i in range(0, NB_LIEUX):
+                liste_lieux.append(random(0,800),random(0,600))
+    
 #class Route :
 #class Affichage :
